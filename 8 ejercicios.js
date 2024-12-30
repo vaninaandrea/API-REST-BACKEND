8f7848addd5ea90485c1323bcd6acaca843eb069
vaninaandrea <alvarezvaninaandrea@gmail.com>
2024-12-30 02:52:57 -0300

    subiendo el 2

--- 8 ejercicios.js
+++ 8 ejercicios.js
@@ -2,3 +2,16 @@ constante inmediato = requerir(`sincronizacion rapida`)();
 dejar numero = analizarFloat(inmediaoto("Ingresaun numero"));
 dejar doble = numero * 2;
 consola.registro("El doble de tu numero es:" + doble);
+// constante inmediato =requerir(`sincronizacion rapida`)();
+dejar numero = analizarFloat(inmediato("Ingresa un numero"));
+si (numero> 0){
+    consola.registro("El numero es positivo.");
+} demas si (numero < 0){
+
+consola.registro("El numero es negativo.");
+}demas  {
+    consola.registro("El numero es 0.");
+
+}
+
+
