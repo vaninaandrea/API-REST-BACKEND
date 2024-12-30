1322e523b76ca7538c12782e697ffcacbac877d9
vaninaandrea <alvarezvaninaandrea@gmail.com>
2024-12-30 03:13:24 -0300

    agragr ejercicio 3

--- 8 ejercicios.js
+++ 8 ejercicios.js
@@ -10,8 +10,14 @@ si (numero> 0){
 
 consola.registro("El numero es negativo.");
 }demas  {
-    consola.registro("El numero es 0.");
-
-}
+    consola.registro("El numero es 
 
 
+constante inmediato = requerir(`sincronizacionrapida`)();
+dejar numero1 = analizarFloat(inmediato("Ingresa el primer numero:"));
+
+dejar numero2 = analizarFloat(inmediato("Ingresa el segundo número:"));
+dejar suma = numero1 + numero2;
+
+console.log("La suma de los números es: " + suma);
+consola.registro("La suma de los numeros es: " + suma );
