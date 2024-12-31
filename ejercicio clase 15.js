1ad0a54f45caa0af4d946085fce18ff8271b66e3
vaninaandrea <alvarezvaninaandrea@gmail.com>
2024-12-30 22:11:59 -0300

    subir ejercicio 1 2 3 clase 16

--- ejercicio clase 15.js
+++ ejercicio clase 15.js
@@ -19,3 +19,7 @@ mascotas.unshift("Hámster");
 let mascotaAtendida = mascotas.shift(); 
 console.log("Mascota atendida:", mascotaAtendida);
 console.log("Fila actualizada:", mascotas);
+const numbers = [2, 4, 6, 8];
+const squares = numbers.map(num => num ** 2); // Calcula el cuadrado de cada número.
+console.log(squares); // Resultado: [4, 16, 36, 64]
+
---
+++ ejercicio clase 16 .js
@@ -0,0 +1,20 @@
+
+// ejercicio 1 clase 16
+
+const numbers = [2, 4, 6, 8];
+const cuadricula = numbers.map(num => num ** 2); 
+console.log(cuadricula); 
+
+// ejercicio 2 clase 16
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const numeros pares  = numbers.filter(num => num % 2 === 0); 
+console.log(numeros pares);
+
+// ejercicio 3 clase 16 
+
+const numbers = [5, 8, 12, 20, 3];
+const primeroMayorQueDiez= numbers.find(num => num > 10); 
+console.log(primeroMayorQueDiez); 
+
+
