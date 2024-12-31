8370c28a1d170010ca63fd0d377782487d62674d
vaninaandrea <alvarezvaninaandrea@gmail.com>
2024-12-30 21:59:32 -0300

    subir ejercicio 1 2 3 clase 15

---
+++ ejercicio clase 15.js
@@ -0,0 +1,21 @@
+
+// ejercicio 1 clase 15
+
+let personajes = ["Link", "Zelda"];
+personajes.push("Mario", "Luigi");
+console.log(personajes);
+
+// ejercicio 2 clase 15
+
+let invitados = ["Ana", "Juan", "Carlos", "Sofía"];
+let invitadoEliminado = invitados.pop(); 
+console.log("Invitado eliminado:", invitadoEliminado);
+console.log("Lista actualizada:", invitados);
+
+// ejercicio 3 clase 15 
+
+let mascotas = ["Perro", "Gato", "Conejo"];
+mascotas.unshift("Hámster"); 
+let mascotaAtendida = mascotas.shift(); 
+console.log("Mascota atendida:", mascotaAtendida);
+console.log("Fila actualizada:", mascotas);
